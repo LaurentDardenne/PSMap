4dcca63d420b3c7e475eed81fc9b42ce930159a2
laurent <ladardenne@orange.fr>
2019-05-03 20:12:35 +0200

    Add Import-module .\Two.ps1 as dotsource Add explicite Ampersand invokeOperator

--- Test/SourceCode/NestedCall/NestedCall.ps1
+++ Test/SourceCode/NestedCall/NestedCall.ps1
@@ -1,8 +1,11 @@
 Write-warning "Call a script"
 .\One.ps1
+&'.\One.ps1'
+#&'C:\Prive\Modules\PSMap\Test\SourceCode\NestedCall\One.ps1'
 Write-warning "Call a dotsourced script"
 . .\Two.ps1
 Function Get-NestedCall {
     Write-warning "script NestedCall -function Get-NestedCall"
 }
 #todo Import-Module
+Import-module .\Two.ps1
