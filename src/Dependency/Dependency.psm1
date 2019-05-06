a6f5b3046369087065d40f14df7817032192cfe2
laurent <ladardenne@orange.fr>
2019-05-06 22:03:35 +0200

    fix encoding fix ConvertTo-FileInfo

--- src/Dependency/Dependency.psm1
+++ src/Dependency/Dependency.psm1
@@ -1,4 +1,4 @@
-# Add-Type -AssemblyName System.ComponentModel.DataAnnotations
+﻿# Add-Type -AssemblyName System.ComponentModel.DataAnnotations
 # [System.ComponentModel.DataAnnotations.KeyAttribute]
 #todo 'identité composite',Unicité ?
 #                             Module Name Or Module,Version,Guid
@@ -17,6 +17,99 @@
 #todo doit pointer sur le contexte de l'appelant
 $sbIsScriptDotSource={ ($_ -is [PSCustomObject]) -and ($_.PsTypenames[0] -eq 'InformationScript') }
 
+
+function Get-AST {
+  #from http://becomelotr.wordpress.com/2011/12/19/powershell-vnext-ast/
+    <#
+    
+    .Synopsis
+       Function to generate AST (Abstract Syntax Tree) for PowerShell code.
+    
+    .DESCRIPTION
+       This function will generate Abstract Syntax Tree for PowerShell code, either from file or direct input.
+       Abstract Syntax Tree is a new feature of PowerShell 3 that should make parsing PS code easier.
+       Because of nature of resulting object(s) it may be hard to read (different object types are mixed in output).
+    
+    .EXAMPLE
+       $AST = Get-AST -FilePath MyScript.ps1
+       $AST will contain syntax tree for MyScript script. Default are used for list of tokens ($Tokens) and errors ($Errors).
+    
+    .EXAMPLE
+       Get-AST -Input 'function Foo { param ($Foo) Write-Host $Foo }' -Tokens MyTokens -Errors MyErors | Format-Custom
+       Display function's AST in Custom View. $MyTokens contain all tokens, $MyErrors would be empty (no errors should be recorded).
+    
+    .INPUTS
+       System.String
+    
+    .OUTPUTS
+       System.Management.Automation.Languagage.Ast
+    
+    .NOTES
+       Just concept of function to work with AST. Needs a polish and shouldn't polute Global scope in a way it does ATM.
+    
+    #>
+    
+    [CmdletBinding(
+        DefaultParameterSetName = 'File'
+    )]
+    param (
+        # Path to file to process.
+        [Parameter(
+            Mandatory,
+            HelpMessage = 'Path to file to process',
+            ParameterSetName = 'File'
+        )]
+        [Alias('Path','PSPath')]
+        [ValidateScript({
+            if (Test-Path -Path $_ -ErrorAction SilentlyContinue) {
+                $true
+            } else {
+                throw "File does not exist!"
+            }
+        })]
+        [string]$FilePath,
+        
+        # Input string to process.
+        [Parameter(
+            Mandatory,
+            HelpMessage = 'String to process',
+            ParameterSetName = 'Input'
+    
+        )]
+        [Alias('Script','IS')]
+        [string]$InputScript,
+    
+        # Name of the list of Errors.
+        [Alias('EL')]
+        [string]$ErrorsList = 'ErrorsAst',
+        
+        # Name of the list of Tokens.
+        [Alias('TL')]
+        [string]$TokensList = 'Tokens'
+    )
+        New-Variable -Name $ErrorsList -Value $null -Scope Global -Force
+        New-Variable -Name $TokensList -Value $null -Scope Global -Force
+    
+    
+        switch ($psCmdlet.ParameterSetName) {
+            File {
+                $ParseFile = (Resolve-Path -Path $FilePath).ProviderPath
+                [System.Management.Automation.Language.Parser]::ParseFile(
+                    $ParseFile, 
+                    [ref](Get-Variable -Name $TokensList),
+                    [ref](Get-Variable -Name $ErrorsList)
+                )
+            }
+            Input {
+              [System.Management.Automation.Language.Parser]::ParseInput(
+                    $InputScript, 
+                    [ref](Get-Variable -Name $TokensList),
+                    [ref](Get-Variable -Name $ErrorsList)
+                )
+            }
+        }
+  }
+  
 function Test-ScriptName{
  param( [System.IO.FileInfo] $Path )
   $Path.Extension -eq '.ps1'
@@ -26,7 +119,14 @@ function ConvertTo-FileInfo{
   #todo voir les difficultés avec les différentes constructions de PSPath
   param ($Path)
 
-  $ScriptPath=Convert-Path $Path
+  try
+  { 
+     #try to convert a relativ path with the current location
+    $ScriptPath=Convert-Path $Path -ErrorAction 'Stop'
+  } catch [System.Management.Automation.ItemNotFoundException] {
+     $ScriptPath=$Path
+     #$ScriptPath.Exists=$False
+  }
   [System.IO.FileInfo]$ScriptPath
 }
 
@@ -139,7 +239,8 @@ function Get-UsingStatementParameter{
 
 function Get-InformationModule{
   param(
-    [System.Management.Automation.Language.CommandAst] $Command
+    [System.Management.Automation.Language.CommandAst] $Command,
+    $Contener
   )
     $CommandElement=$Command.CommandElements[1]
     $TypeName=$CommandElement.GetType().Name
@@ -158,6 +259,7 @@ function Get-InformationModule{
                                         #todo use case: Import-module c:\temp\modules\my.dll
                                         # si c'est un module binaire ok, si c'est une dll dotnet sans déclaration de cmdlet
                                         #alors c'est une 'erreur' d'utilisation de IPMO comme IPMO File.sp1 peut l'être (confusion)
+                                        todo différencier nom d'un nom de chemin full ou relatif
                                         $FileInfo=ConvertTo-FileInfo $StaticParameters.Name
                                         if  (Test-ScriptName $FileInfo)
                                         { 
