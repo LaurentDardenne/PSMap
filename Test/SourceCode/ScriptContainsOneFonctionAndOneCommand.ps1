1d89c07b0df75cdd27a557784cd5f0c8a44eab30
laurent <ladardenne@orange.fr>
2019-08-18 13:06:35 +0200

    add comment

--- Test/SourceCode/ScriptContainsOneFonctionAndOneCommand.ps1
+++ Test/SourceCode/ScriptContainsOneFonctionAndOneCommand.ps1
@@ -1,3 +1,7 @@
+#digraph ne connait que la fonction, et on ne peut avoir qu'une seule entrée de même nom
+#Ici on ne peut donc avoir la définition et l'appel
+# première référence de fonction 'name' -> add AST F° : function:Name
+# second référence 'Callname', un appel -> recherche function:Callname 
 function Set-FileWrong
 {
     [CmdletBinding(SupportsShouldProcess=$true)]
