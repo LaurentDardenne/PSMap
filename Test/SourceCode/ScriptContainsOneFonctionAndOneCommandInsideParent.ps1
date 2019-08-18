cc1b9613dd9007ceca5f22d3049e3c7862278969
laurent <ladardenne@orange.fr>
2019-08-18 13:15:02 +0200

    add use cases

---
+++ Test/SourceCode/ScriptContainsOneFonctionAndOneCommandInsideParent.ps1
@@ -0,0 +1,14 @@
+Function Parent {
+    function Set-FileWrong
+    {
+        [CmdletBinding(SupportsShouldProcess=$true)]
+        Param
+        (
+            # Path to file
+            [Parameter(Mandatory=$true)]
+            $Path
+        )
+        'test'
+    }
+    Set-FileWrong -Path 'c:\'
+}
